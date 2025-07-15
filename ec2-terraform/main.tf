5301e5100a9b07abc555f5bb5368eb37944fe899
HarshwardhanPatil07 <harshwardhanpatil07@yahoo.com>
2025-07-15 18:39:18 +0530

    VPC Created with subnets and Internet Gateway for requests

---
+++ ec2-terraform/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name: "${var.env_prefix}-vpc" #dev-vpc
+    }
+}
+
+resource "aws_subnet" "myapp-subnet" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
+    tags = {
+        Name: "${var.env_prefix}-subnet-1" #dev-subnet-1
+    }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name: "${var.env_prefix}-rtb" 
+    }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    tags = {
+        Name: "${var.env_prefix}-igw" 
+    }
+}
