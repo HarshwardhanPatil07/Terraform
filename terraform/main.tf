6396de0b7b4f391faaea8f359a669a390ac254b1
HarshwardhanPatil07 <harshwardhanpatil07@yahoo.com>
2025-07-15 09:24:54 +0530

    Data sources and Resource in tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,13 +8,19 @@ provider "aws" {
   # Configuration for the resource
 // }
 resource "aws_vpc" "development-vpc" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name: "development"
+      }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id = aws_vpc.development-vpc.id
   cidr_block = "10.0.10.0/24"
   availability_zone = "ap-south-1a"
+  tags = {
+    Name: "subnet-1-dev"
+  }
 }
 
 // https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
@@ -26,6 +32,9 @@ resource "aws_subnet" "dev-subnet-2" {
   vpc_id = data.aws_vpc.existing_vpc.id
   cidr_block = "172.31.48.0/20"
   availability_zone = "ap-south-1a"
+  tags = {
+    Name: "subnet-2-default"
+  }
 }
 
 # done till 1 VPC and 2 subnets
