16839f195fe0b199d95a0cc3a06899572ef4bc54
HarshwardhanPatil07 <harshwardhanpatil07@yahoo.com>
2025-07-19 19:29:37 +0530

    Added user_data script so don't need to ssh manually

--- ec2-terraform/main.tf
+++ ec2-terraform/main.tf
@@ -8,7 +8,8 @@ variable avail_zone {}
 variable env_prefix {}
 variable my_ip {}
 variable instance_type {}
-
+# variable my_public_key {}
+variable public_key_location {}
 
 
 resource "aws_vpc" "myapp-vpc" {
@@ -134,6 +135,17 @@ data "aws_ami" "latest-amazon-linux-image" {
 output "aws_ami_id" {
     value = data.aws_ami.latest-amazon-linux-image.id
 }
+
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip # public ip from `terraform state show aws_instance.myapp-server`
+}
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    # public_key = "var.my_public_key"
+    public_key = file(var.public_key_location)
+}
+
 resource "aws_instance" "myapp-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
@@ -143,8 +155,19 @@ resource "aws_instance" "myapp-server" {
     availability_zone = var.avail_zone
 
     associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
 
-    key_name = "server-key-pair"
+    user_data = file("entry-script.sh")
+    # or can directly write below like this
+    
+    # user_data = <<EOF
+    # #!/bin/bash
+    # sudo yum update -y && sudo yum install -y docker
+    # sudo systemctl start docker
+    # sudo usermod -aG docker ec2-user # we want to run docker commands without sudo
+    # docker run -p 8080:80 nginx
+    # EOF 
+
     tags = {
         Name = "${var.env_prefix}-server" 
     }
